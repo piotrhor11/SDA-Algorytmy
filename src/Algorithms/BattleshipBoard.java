bb58056c39acb044dd57dfb2fc72bb90905d83ff
piotrhor11 <piotrhor11@gmail.com>
2017-08-06 08:53:57 +0200

    IntelliJ: //ToCheck pattern added BattleshipBoard: Comments added

--- src/Algorithms/BattleshipBoard.java
+++ src/Algorithms/BattleshipBoard.java
@@ -4,7 +4,7 @@ package Algorithms;
 public class BattleshipBoard extends AbstractAlgorithm {
 
     //    private final char[] alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'Z'};
-    private final char[] alphabet = {'A', 'B', 'C'};
+    private final char[] alphabet = {'A', 'B', 'C'};                //Zmienione w celach testowych
 
 
     @Override
@@ -20,13 +20,16 @@ public class BattleshipBoard extends AbstractAlgorithm {
         if (Math.sqrt(size) % 1 == 0.0) {
 
             int side = (int) Math.sqrt(size);
-            for (int i = 0; i < side; i++) {                    //ToDo - "Excel" extension not ready
+            for (int i = 0; i < side; i++) {                    //ToCheck - nie wiem jak zautomatyzować tworzenie się odpowiednich konstrukcji if, myślałem o rekurencji ale trochę się to komplikuje
                 for (int j = 0; j < side; j++) {
+
+//STARY KOD
 //                    for (int k = 0; k <= (i / (int) Math.pow(alphabet.length, 1d)); k++) {            //Zamiast 1d powinny być wskaźniki kolejnych potęg (1d, 2d, 3d, itd.) jeżeli i przekroczy wartość danej potęgi. Niestety nie umiem tego zaimplementować. :(
 //                        System.out.printf("%c", alphabet[i % alphabet.length]);
 //                    }
+//STARY KOD KONIEC
 
-                    // ...  - kolejne konstrukcje "if" narastają z dołu do góry w nieskończoność zgodnie z zależnością wynikającą z poniższych 4  instrukcji if (dokładniej to zależność jest stała [idąć w górę] od 2-go ifa od końca )
+                    // ...  - kolejne konstrukcje "if" narastają tutaj idąc z dołu do góry w nieskończoność zgodnie z zależnością wynikającą z poniższych 4  instrukcji if (dokładniej to zależność jest stała [idąć w górę] od 2-go ifa po koniec Wszechświata )
 
                     if (0 < ((i - (int) Math.pow(alphabet.length, 2d) - (int) Math.pow(alphabet.length, 1d)) / (int) Math.pow(alphabet.length, 3d))) {
                         System.out.printf("%c", alphabet[((i - (int) Math.pow(alphabet.length, 3d) - (int) Math.pow(alphabet.length, 2d) - (int) Math.pow(alphabet.length, 1d)) / ((int) Math.pow(alphabet.length, 3d))) % alphabet.length]);
