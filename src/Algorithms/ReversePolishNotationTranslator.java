bc42078e6d5788d1c835fbc1bb59cc5f610b22f5
piotrhor11 <piotrhor11@gmail.com>
2017-08-08 21:00:09 +0200

    ReversePolishNotationTranslator: some code written down from classes, work in progress

--- src/Algorithms/ReversePolishNotationTranslator.java
+++ src/Algorithms/ReversePolishNotationTranslator.java
@@ -15,12 +15,20 @@ public class ReversePolishNotationTranslator extends AbstractAlgorithm {
         Stack<String> stosik = new Stack<>();
         ArrayList<Integer> variables = new ArrayList<>();
 
+        String helper = "";
+
         for (int i = 1; i < input.length; i++)
 
             switch (input[i]) {
                 case "(":
+                    stosik.push(input[i]);
                     break;
                 case ")":
+                    helper = stosik.pop();
+                    while (!helper.equals("(")) {
+                        System.out.printf("%s", helper);
+                        helper = stosik.pop();
+                    }
                     break;
                 case "+":
                     break;
@@ -30,12 +38,41 @@ public class ReversePolishNotationTranslator extends AbstractAlgorithm {
                     break;
                 case "/":
                     break;
-                case "=":
+                case "^":
+                    while (!stosik.empty()) {
+                        if (GetOperatorPriority(input[i]) == 3 || GetOperatorPriority(input[i]) >= GetOperatorPriority(stosik.peek())) {
+                            break;
+                        } else {
+                            System.out.printf("%s", stosik.pop());
+                        }
+                    }
                     break;
-
+                case "=":
+                    while (!stosik.empty()) {
+                        System.out.printf("%s", stosik.pop());
+                    }
+                    break;
+                default:
+                    System.out.printf("%s", input[i]);
+                    break;
 
 
             }
 
+    }
+
+    public int GetOperatorPriority(String operator) throws Exception {
+        switch (operator) {
+            case "+":
+            case "-":
+                return 1;
+            case "*":
+            case "/":
+                return 2;
+            case "^":
+                return 3;
+            default:
+                throw new Exception("Nierozpoznany operator!");
+        }
     }
 }
